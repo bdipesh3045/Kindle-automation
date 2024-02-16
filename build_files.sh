71cfaaa601376e488870e6c18a438bce3f563bef
Dipesh Sharma <bdipesh304@gmail.com>
2024-02-17 00:06:27 +0545

    packages update

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
 
 pip install --upgrade pip
 pip uninstall setuptools
-pip3 uninstall setuptools
+
 pip install setuptools
-pip3 install setuptools
+
 git clone https://github.com/rsgalloway/instapaper.git
 cd instapaper
 python setup.py install
